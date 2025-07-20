fe4713cc9b742747a2324be23fa2ddedb76a6f19
daeun <3v3r.alp@gmail.com>
2025-07-21 01:42:23 +0900

    css 작성

---
+++ scripts/index.js
@@ -0,0 +1,39 @@
+const lifeBg = document.querySelectorAll('.life_bg');
+const lifeSpan = document.querySelectorAll('.life_bg span');
+const lifeContainer = document.querySelectorAll('.life_contain');
+console.log(lifeBg)
+
+//1행 life
+lifeContainer.forEach((obj,idx)=>{
+    obj.addEventListener('mouseenter',()=>{
+        lifeContainer.forEach((life)=>{
+            for(let i of lifeBg){i.style.backgroundColor = 'rgba(0,0,0,0.7)'};
+            for(let i of lifeSpan){i.style.display = 'none';};
+            life.style.width = '190px';
+        });
+        lifeBg[idx].style.backgroundColor = 'transparent';
+        lifeSpan[idx].style.display = 'inline';
+        obj.style.width = '800px';
+    });
+    obj.addEventListener('mouseleave',()=>{
+        lifeBg[idx].style.backgroundColor = 'rgba(0,0,0,0.7)';
+        lifeSpan[idx].style.display = 'none';
+        obj.style.width = '190px';
+    })
+})
+
+//2행 기능
+const keySwiper = new Swiper('.swiper_key_features',{
+    slidesPerView:4.5,
+    spaceBetween:60,
+    autoplay:{delay:2000},
+    loop:true,
+    direction:'horizontal',
+    speed:5000,
+});
+/* keySwiper.addEventListener('mouseenter',()=>{
+    keySwiper.autoplay.stop(); 
+}) */
+document.getElementsByClassName('.swiper_key_features',()=>{
+    keySwiper.autoplay.stop();
+})
